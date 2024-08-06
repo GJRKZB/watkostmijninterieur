1a1768ffca76dc71f8c828e05b3da3b2d9126ce5
GJRKZB <Robinkonijnendijk@gmail.com>
2024-08-06 17:51:20 +0200

    disable scrolling page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { FormProvider } from "./context/FormContext";
 export default function Home() {
   return (
     <FormProvider>
-      <div className="flex h-screen w-screen flex-col items-center justify-center bg-slate-300 px-5 py-24 md:p-40">
+      <div className="flex h-screen w-screen flex-col items-center justify-center overflow-hidden bg-slate-300 px-5 py-24 md:p-40">
         <MultiStepForm />
       </div>
     </FormProvider>
